34912193db9e0669f5b06fd07a4493e30ec72452
Akash <akashniki@gmail.com>
2019-08-02 10:42:25 +0000

    axon7: PixelExperience Bringup

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -76,7 +76,7 @@ BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_HEADER_ARCH := arm64
 TARGET_KERNEL_SOURCE := kernel/zte/msm8996
-TARGET_KERNEL_CONFIG := lineageos_axon7_defconfig
+TARGET_KERNEL_CONFIG := prelude_defconfig
 TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
 
 # Qualcomm support
